1e96b635836ca5441976b74a851d70b7999336e7
william1010199 <lopezwilliam177@gmail.com>
2023-10-18 19:11:58 -0300

    add interface

--- entrenamiento2.py
+++ entrenamiento2.py
@@ -2,59 +2,20 @@ import face_recognition
 import cv2
 import numpy as np
 from tkinter import *
+from tkinter import ttk  # Importar ttk para Treeview
 import os
 import pickle
-import threading
 
-data_path = 'images_data'
+data_path = 'images'
 model_path = 'trained_model.pkl'
 
 root = Tk()
+root.title("Reconocimiento Facial")
 
 name_var = StringVar()
-capturing = False  # Variable para indicar si se está capturando o no
-cam = cv2.VideoCapture(0)  # Inicializar la cámara
-thread = None  # Hilo para la captura de video
 
-def take_picture(name):
-    person_folder = os.path.join(data_path, name)
-    if not os.path.exists(person_folder):
-        os.makedirs(person_folder)
 
-    ret, frame = cam.read()
-    img_name = os.path.join(person_folder, f"{name}_img.jpg")
-    cv2.imwrite(img_name, frame)
-    print(f"Image saved for {name}!")
-
-def start_capture():
-    global capturing
-    name = name_var.get().strip()
-    if not name:
-        print("Por favor, ingrese un nombre antes de capturar imágenes.")
-        return
-
-    while capturing:
-        ret, frame = cam.read()
-
-        # Mostrar el video en la ventana
-        cv2.imshow('Video', frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('c'):  # Presionar 'c' para capturar
-            take_picture(name)
-
-    # Restaurar el video al cerrar la captura
-    cv2.imshow('Video', frame)
-
-def capture_video():
-    global capturing, thread
-    if thread is None or not thread.is_alive():
-        capturing = True
-        thread = threading.Thread(target=start_capture)
-        thread.start()
-
-def stop_capture():
-    global capturing
-    capturing = False
+# Función para tomar fotografías
 
 def train_model():
     encodings = []
@@ -81,6 +42,30 @@ def train_model():
 
     print('Modelo entrenado y guardado.')
 
+# Función para probar el modelo
+# Función para tomar fotografías
+def take_picture(name):
+    person_folder = os.path.join(data_path, name)
+    if not os.path.exists(person_folder):
+        os.makedirs(person_folder)
+
+        cam = cv2.VideoCapture(0)
+        img_counter = 0
+
+        while True:
+            ret, frame = cam.read()
+
+            if img_counter == 10:
+                break
+
+            img_name = os.path.join(person_folder, f"img_{img_counter}.jpg")
+            cv2.imwrite(img_name, frame)
+            print(f"Imagen {img_counter} guardada para {name}!")
+            img_counter += 1
+
+        cam.release()
+
+# Función para probar el modelo
 def test_model():
     with open(model_path, 'rb') as model_file:
         model_data = pickle.load(model_file)
@@ -88,15 +73,14 @@ def test_model():
     known_encodings = model_data['encodings']
     known_names = model_data['names']
 
-    cam = cv2.VideoCapture(0)
-
-    while True:
+    def update_gui():
         ret, frame = cam.read()
 
         face_locations = face_recognition.face_locations(frame)
         face_encodings = face_recognition.face_encodings(frame, face_locations)
 
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        for i, (top, right, bottom, left) in enumerate(face_locations):
+            face_encoding = face_encodings[i]
             matches = face_recognition.compare_faces(known_encodings, face_encoding)
             name = "Desconocido"
 
@@ -104,9 +88,23 @@ def test_model():
                 first_match_index = matches.index(True)
                 name = known_names[first_match_index]
 
-            color = (0, 255, 0)  # Por defecto, verde para personas conocidas
+                # Buscar si la persona ya existe en la Treeview
+                item_id = None
+                for item in result_tree.get_children():
+                    if result_tree.item(item, "values")[0] == name:
+                        item_id = item
+                        break
 
-            if name == "Desconocido":
+                # Insertar o actualizar la entrada en la Treeview
+                if item_id is None:
+                    result_tree.insert("", "end", values=(name, "Conocido"))
+                else:
+                    result_tree.item(item_id, values=(name, "Conocido"))
+
+                color = (0, 255, 0)  # Por defecto, verde para personas conocidas
+            else:
+                # Agregar a la Treeview como desconocido
+                result_tree.insert("", "end", values=("Desconocido", "Desconocido"))
                 color = (0, 0, 255)  # Rojo para personas desconocidas
 
             cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
@@ -116,34 +114,61 @@ def test_model():
         cv2.imshow('Reconocimiento Facial en Tiempo Real', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            cam.release()
+            cv2.destroyAllWindows()
+        else:
+            # Programar la próxima actualización después de 10 milisegundos
+            root.after(10, update_gui)
 
-    cam.release()
-    cv2.destroyAllWindows()
+    cam = cv2.VideoCapture(0)
+    # Iniciar la primera actualización después de 10 milisegundos
+    root.after(10, update_gui)
 
-def close_app():
-    global capturing
-    capturing = False
-    if thread and thread.is_alive():
-        thread.join()  # Esperar a que el hilo termine
-    cam.release()
-    cv2.destroyAllWindows()
-    root.destroy()
+# Frame para la captura de imágenes
+capture_frame = Frame(root, padx=10, pady=10)
+capture_frame.pack()
 
-name_entry = Entry(root, textvariable=name_var, width=30)
-name_entry.pack()
+name_label = Label(capture_frame, text="Nombre:")
+name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-btn_capture_video = Button(root, text="Capturar desde Video", command=capture_video)
-btn_capture_video.pack()
+name_entry = Entry(capture_frame, textvariable=name_var, width=20)
+name_entry.grid(row=0, column=1, padx=5, pady=5)
 
-btn_stop_capture = Button(root, text="Detener Captura", command=stop_capture)
-btn_stop_capture.pack()
+def capture_images():
+    name = name_var.get().strip()
+    if not name:
+        print("Por favor, ingrese un nombre antes de capturar imágenes.")
+        return
 
-btn_train = Button(root, text="Entrenar", command=train_model)
-btn_train.pack()
+    take_picture(name)
 
-btn_test = Button(root, text="Probar Modelo", command=test_model)
-btn_test.pack()
+capture_btn = Button(capture_frame, text="Capturar Imágenes", command=capture_images)
+capture_btn.grid(row=0, column=2, padx=5, pady=5)
 
-root.protocol("WM_DELETE_WINDOW", close_app)  # Capturar evento de cierre de ventana
+# Frame para el entrenamiento del modelo
+train_frame = Frame(root, padx=10, pady=10)
+train_frame.pack()
+
+train_label = Label(train_frame, text="Entrenamiento del Modelo:")
+train_label.grid(row=0, column=0, padx=5, pady=5)
+
+train_btn = Button(train_frame, text="Entrenar Modelo", command=train_model)
+train_btn.grid(row=0, column=1, padx=5, pady=5)
+
+# Frame para la prueba del modelo
+test_frame = Frame(root, padx=10, pady=10)
+test_frame.pack()
+
+test_label = Label(test_frame, text="Prueba del Modelo:")
+test_label.grid(row=0, column=0, padx=5, pady=5)
+
+# Crear Treeview para mostrar resultados
+result_tree = ttk.Treeview(test_frame, columns=("Name", "Status"), show="headings")
+result_tree.heading("Name", text="Nombre")
+result_tree.heading("Status", text="Estado")
+result_tree.grid(row=1, column=0, padx=5, pady=5)
+
+test_btn = Button(test_frame, text="Probar Modelo", command=test_model)
+test_btn.grid(row=0, column=1, padx=5, pady=5)
+
 root.mainloop()
